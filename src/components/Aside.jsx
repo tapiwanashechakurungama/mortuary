9cae6432f7025367c7e10e75ca9cb12433fa784a
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2025-02-13 11:52:07 +0200

    adding profiles

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,103 +3,100 @@ import {  GiClawSlashes, GiMailbox, GiMayanPyramid, GiPhone } from "react-icons/
 const Aside = () => {
   return (
     <aside className="sidebar">
-        <div className="sidebar-info">
-          <figure className="avatar-box">
-            <img
-              src={"/images/declan id photo.jpg"}
-              alt="Richard hanrick"
-              width="80"
-            />
-          </figure>
+      <div className="sidebar-info">
+        <figure className="avatar-box">
+          <img
+            src={"/images/declan id photo.jpg"}
+            alt="Richard hanrick"
+            width="80"
+          />
+        </figure>
 
-          <div className="info-content">
-            <h1 className="name" title="kudakwashe gudyanga">
-              Bryan Vengwa
-            </h1>
+        <div className="info-content">
+          <h1 className="name" title="kudakwashe gudyanga">
+            Bryan Vengwa
+          </h1>
 
-            <p className="title">Software Engineer</p>
-          </div>
-
-          <button className="info_more-btn" data-sidebar-btn>
-            <span>Show Contacts</span>
-
-            <GiClawSlashes />
-          </button>
+          <p className="title">Software Engineer</p>
         </div>
 
-        <div className="sidebar-info_more">
-          <div className="separator"></div>
+        <button className="info_more-btn" data-sidebar-btn>
+          <span>Show Contacts</span>
 
-          <ul className="contacts-list">
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiMailbox/>
-                
-              </div>
+          <GiClawSlashes />
+        </button>
+      </div>
 
-              <div className="contact-info">
-                <p className="contact-title">Email</p>
+      <div className="sidebar-info_more">
+        <div className="separator"></div>
 
-                <a href="kudakwashegudyanga41@gmail.com" className="contact-link">
-                  kudakwashegudyanga41@gmail.com
-                </a>
-              </div>
-            </li>
+        <ul className="contacts-list">
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiMailbox />
+            </div>
 
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiPhone/>
-              </div>
+            <div className="contact-info">
+              <p className="contact-title">Email</p>
 
-              <div className="contact-info">
-                <p className="contact-title">Phone</p>
-
-                <a href="tel:+263780121672" className="contact-link">
-                  +263780121672
-                </a>
-              </div>
-            </li>
-
-         
-
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiMayanPyramid/>
-                <ion-icon name="location-outline"></ion-icon>
-              </div>
-
-              <div className="contact-info">
-                <p className="contact-title">Location</p>
-
-                <address>Harare Zimbabwe</address>
-              </div>
-            </li>
-          </ul>
-
-          <div className="separator"></div>
-
-          <ul className="social-list">
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-facebook"></ion-icon>
+              <a href="kudakwashegudyanga41@gmail.com" className="contact-link">
+                bryanvengwa@uncommon.org
               </a>
-            </li>
+            </div>
+          </li>
 
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-twitter"></ion-icon>
-              </a>
-            </li>
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiPhone />
+            </div>
 
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-instagram"></ion-icon>
+            <div className="contact-info">
+              <p className="contact-title">Phone</p>
+
+              <a href="tel:+263786439013" className="contact-link">
+                +263 78 643 9013
               </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
-  )
+            </div>
+          </li>
+
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiMayanPyramid />
+              <ion-icon name="location-outline"></ion-icon>
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Location</p>
+
+              <address>Harare Zimbabwe</address>
+            </div>
+          </li>
+        </ul>
+
+        <div className="separator"></div>
+
+        <ul className="social-list">
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a href="#" className="social-link">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 }
 
 export default Aside
